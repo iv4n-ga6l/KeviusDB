bcd75c34c25817827ee39f9e1988b44d8d6e34dc
Ivan <apedoivan@gmail.com>
2025-12-14 23:25:42 +0100

    Fixed versionning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="keviusdb",
-    version="1.0.10",
+    version="1.1.0",
     description="Persistent Memory for AI Agents | Fast Key-Value Storage with Agent Memory capabilities",
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
